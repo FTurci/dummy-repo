a519be21e7596174d0297a2d9a6637e47f9739d3
hughf2468 <159436335+hughf2468@users.noreply.github.com>
2024-02-08 11:46:37 +0000

    massive improvement

--- leap_year_calculator.py
+++ leap_year_calculator.py
@@ -1,5 +1,5 @@
 
-this_year = 2022
+this_year = 2024
 
 start_year = 2010
 end_year = 2026
@@ -7,15 +7,15 @@ end_year = 2026
 for year in range(start_year, end_year):
     
     if year%4 == 0 and year%400 == 0:
-        leap_year = "Yes"
+        leap_year = "The excellent leap_year_calculator has determined this year to be a leap year"
     elif year%4 == 0 and year%100 == 0:
-        leap_year = "No"
+        leap_year = "The excellent leap_year_calculator has determined this year to not be a leap year"
     elif year%4 == 0:
-        leap_year = "Yes"
+        leap_year = "The excellent leap_year_calculator has determined this year to be a leap year"
     else:
-        leap_year = "No"
+        leap_year = "The excellent leap_year_calculator has determined this year to not be a leap year"
     
     if year == this_year:
-        print(f"Is this year a leap year? {leap_year}")
+        print(f"Does the superb leap_year_calculator determine this year to be a leap year? {leap_year}")
     else:
-        print(f"Is {year} a leap year? {leap_year}")
+        print(f"Does the superb leap_year_calculator determine {year} to be a leap year? {leap_year}")
